f4f0c9a88b33ce13380049a17b1aa5ab097afadd
Israel <israel.toledo@outlook.es>
2025-08-08 16:38:41 -0600

    Fixed mod updater files handler

--- ModTool/PythonFiles/constructionUpdater.py
+++ ModTool/PythonFiles/constructionUpdater.py
@@ -110,9 +110,9 @@ class ConstructionUpdaterUI(QWidget):
     def updateMod(self):
 
         #Moded files after restoring constructions
-        architectureJson = loadJson(self.buildPath("moded", "Architecture.json"))
+        architectureJson = loadJson(self.buildPath("vanilla", "Architecture.json"))
         DT_ConstructionRecipesJson = loadJson(self.buildPath("moded", "DT_ConstructionRecipes.json"))
-        DT_ConstructionsJson = loadJson(self.buildPath("moded", "DT_Constructions.json"))
+        DT_ConstructionsJson = loadJson(self.buildPath("vanilla", "DT_Constructions.json"))
 
         #Files containing new constructions
         newArchitectureJson = loadJson(self.buildPath("new", "Architecture.json"))
